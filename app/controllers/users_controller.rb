0b4ae8cf14632917322f5682d8960d04b98482fd
Momozono <tanatana1089@gmail.com>
2015-05-09 18:31:25 +0900

    Finish Nineth Chapter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
+    before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :correct_user,   only: [:edit, :update]
+    before_action :admin_user,     only: :destroy
+    #上の2行で、edit,updateアクションを起こす前に、必ずサインインしており、かつ自分自身であるという
+    #確認をしている。
+
+    def index
+        @users = User.paginate(page: params[:page])
+    end
 
 	def show
 		@user = User.find(params[:id])
 	end
+
+    def destroy
+        User.find(params[:id]).destroy
+        flash[:success] = "User destroyed."
+        redirect_to users_url
+    end
 
     def new
     	@user = User.new
@@ -20,12 +35,42 @@ class UsersController < ApplicationController
     	end
     end
 
+    def edit
+        #@user = User.find(params[:id])
+    end
+
+    def update
+        #@user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+
     private
 
-    def user_params
-    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    	#ここでStrong Parameterの概念を取り込んでいる。必要なパラメータのみ与えている。
-    end
+        def user_params
+        	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        	#ここでStrong Parameterの概念を取り込んでいる。必要なパラメータのみ与えている。
+        end
+        #Before actions
+        def signed_in_user #サインインしているか？
+            unless signed_in?
+                store_location
+                redirect_to signin_url, notice: "Please sign in."
+            end
+        end
+
+        def correct_user #サインインしているのは本人か？
+            @user = User.find(params[:id])
+            redirect_to(root_path) unless current_user?(@user)
+        end
+
+        def admin_user
+            redirect_to(root_path) unless current_user.admin?
+        end
 end
 
 
